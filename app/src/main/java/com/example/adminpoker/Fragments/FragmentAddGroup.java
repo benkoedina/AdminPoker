d9069247b47a3d081192fd5b17895290da4db112
benkoedina <benkoedina98@gmail.com>
2019-11-28 13:34:33 +0200

    Adding Groups Successfully

--- app/src/main/java/com/example/adminpoker/Fragments/FragmentAddGroup.java
+++ app/src/main/java/com/example/adminpoker/Fragments/FragmentAddGroup.java
@@ -6,9 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.adminpoker.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -16,9 +19,11 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentAddGroup extends Fragment {
 
+
     Button bt_add;
     EditText et_groupid;
     public  FragmentAddGroup(){};
+    static String status;
 
 
     @Override
@@ -30,8 +35,22 @@ public class FragmentAddGroup extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_add_group, container, false);
 
+
         bt_add = view.findViewById(R.id.bt_add_group);
         et_groupid = view.findViewById(R.id.et_groupid);
+
+        RadioGroup radioGroup = view.findViewById(R.id.groupradio);
+        if (radioGroup != null) {
+            radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                    status= (R.id.activ == checkedId) ? "true" : "false";
+                    Toast.makeText(getContext(), status, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
 
         bt_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,8 +63,21 @@ public class FragmentAddGroup extends Fragment {
 
                 else {
 
+                    Boolean s;
+                    Bundle bundle = new Bundle();
+                    if(status.equals("true"))
+                    {
+                        s = true;
+                    }
+                    else
+                    {
+                        s=false;
+                    }
+                    bundle.putBoolean("status",s);
+                    bundle.putString("group_id",et_groupid.getText().toString());
                     FragmentAddQuestion addQuestion = new FragmentAddQuestion();
                     FragmentTransaction fr = getFragmentManager().beginTransaction();
+                    addQuestion.setArguments(bundle);
                     fr.replace(R.id.container, addQuestion);
                     fr.addToBackStack(null);
                     fr.commit();
--- app/src/main/java/com/example/adminpoker/Fragments/FragmentAddQuestion.java
+++ app/src/main/java/com/example/adminpoker/Fragments/FragmentAddQuestion.java
@@ -1,19 +1,32 @@
 package com.example.adminpoker.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.example.adminpoker.Models.Group;
+import com.example.adminpoker.Models.Question;
 import com.example.adminpoker.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentAddQuestion extends Fragment {
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
-
+    private String status1,status2,status3,status4;
 
     public FragmentAddQuestion(){}
 
@@ -25,6 +38,95 @@ public class FragmentAddQuestion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_add_question, container, false);
+        firebaseDatabase = firebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        Button bt_add = view.findViewById(R.id.bt_add_question);
+        final EditText et_q1 = view.findViewById(R.id.et_question1);
+        final EditText et_q2 = view.findViewById(R.id.et_question2);
+        final EditText et_q3 = view.findViewById(R.id.et_question3);
+        final EditText et_q4 = view.findViewById(R.id.et_question4);
+
+
+
+        RadioGroup radioGroup1 = view.findViewById(R.id.groupradio1);
+        if (radioGroup1 != null) {
+            radioGroup1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                    status1 = (R.id.activ1 == checkedId) ? "active" : "inactive";
+                    Toast.makeText(getContext(), status1, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        RadioGroup radioGroup2 = view.findViewById(R.id.groupradio2);
+        if (radioGroup2 != null) {
+            radioGroup2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                    status2= (R.id.activ2 == checkedId) ? "active" : "inactive";
+                    Toast.makeText(getContext(), status2, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        RadioGroup radioGroup3 = view.findViewById(R.id.groupradio3);
+        if (radioGroup3 != null) {
+            radioGroup3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                    status3 = (R.id.activ3 == checkedId) ? "active" : "inactive";
+                    Toast.makeText(getContext(), status3, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        RadioGroup radioGroup4 = view.findViewById(R.id.groupradio4);
+        if (radioGroup4 != null) {
+            radioGroup4.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                    status4= (R.id.activ4 == checkedId) ? "active" : "inactive";
+                    Toast.makeText(getContext(), status4, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        Bundle bundle = this.getArguments();
+        final String group_id = bundle.getString("group_id");
+        final Boolean group_status = bundle.getBoolean("status");
+        bt_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String q1 = et_q1.getText().toString();
+                String q2 = et_q2.getText().toString();
+                String q3 = et_q3.getText().toString();
+                String q4 = et_q4.getText().toString();
+
+                ArrayList<Question> questions = new ArrayList<>();
+                Question question1 = new Question(0,q1,status1);
+                Question question2 = new Question(1,q2,status2);
+                Question question3 = new Question(2,q3,status3);
+                Question question4 = new Question(3,q4,status4);
+
+                questions.add(question1);
+                questions.add(question2);
+                questions.add(question3);
+                questions.add(question4);
+
+                Group group = new Group(group_id,group_status,questions);
+                databaseReference.child(group.getId()).setValue(group);
+
+                Toast.makeText(getContext(),"Added Successfully",Toast.LENGTH_LONG).show();
+
+
+                Log.d("GROUP", group.toString());
+            }
+        });
+
+
+
 
 
         return view;
